5fda513ade5c8672cd64f95173bca38ae55ed245
Andres M <andresmoraleslopez@gmail.com>
2023-07-17 12:00:11 -0500

    Grid escentials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def grid
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'pages/grid', to: 'pages#grid', as: 'pages_grid'
   # Defines the root path route ("/")
   # root "articles#index"
 end
